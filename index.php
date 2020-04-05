93a27190f297fd527a6a30fecdfb302b414cfe60
Denis <marshaldenison@gmail.com>
2020-04-05 15:37:39 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> veuillez-nous contacter </title>
+	<link rel="stylesheet" href="css/style.css">
+	 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="MARSHALDENISON"></script> 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	
+</head>
+<body style="background: #ffa500;">
+<div class="container-fluid">
+	<div class="diveder"></div>
+	 <div class="heading" > <h2> Un model de formulaire </h2> </div>
+	 
+	   <!-- <h1> Marshaldenison </h1>
+	   <p> je suis etudiant à l'Université Virtuelle de CI </p> -->
+	
+	
+		<div class="row">
+			<div class=" col-lg-10 col-lg-offset-1">
+				<form id="contact-form" method="POST" action="" role="form">
+					<div class="row">
+
+						<div class="col-md-6">
+							<label for="firstname">Nom <span class="red"> * </span></label>
+							<input type="text" id="nom" name="nom" class="form-control" placeholder="votre nom">
+							<p class="comments"> Veuillez bien enregistrer votre Nom svp! </p>
+						</div>
+
+						<div class="col-md-6">
+							<label for="secondname">Prenom <span class="red"> * </span></label>
+							<input type="text" id="prenom" name="prenom" class="form-control" placeholder="votre prenom">
+							<p class="comments"> Veuillez bien enregistrer votre Prenom svp! </p>
+						</div>
+
+						<div class="col-md-6">
+							<label yourage>Age <span class="red"> * </span></label>
+							<input type="text"  id="age" name="age" class="form-control" placeholder="votre age">
+							<p class="comments"> Veuillez bien enregistrer votre Age svp! </p>
+						</div>
+
+						<div class="col-md-6">
+							<label for="youremail">Email <span class="red"> * </span></label>
+							<input type="text" id="email" name="email" class="form-control" placeholder="votre email">
+							<p class="comments"> Veuillez bien enregistrer votre Email svp! </p>
+						</div>
+
+                        <div class="col-md-6">
+							<label for="yournumber">Telephone <span class="red"> * </span></label>
+							<input type="text" id="phone" name="phone" class="form-control" placeholder="votre telephone">
+							<p class="comments"> Veuillez bien enregistrer votre Number svp! </p>
+						</div>
+
+                        <div class="col-md-6">
+							<label for="yourmatricule">Matricule <span class="red"> * </span></label>
+							<input type="text" id="matricle" name="matricule" class="form-control" placeholder="votre matricule">
+							<p class="comments"> Veuillez bien enregistrer votre Matricule svp! </p>
+						</div>
+
+
+                        <div class="col-md-12">
+							<label for="message">Message <span class="red"> * </span></label>
+							<textarea name="" class="form-control"></textarea>
+							<p class="comments"> Veuillez bien ecrire votre message  svp! </p>
+						</div>
+
+						<div class="col-md-12">
+							<input type="submit" class="button"  value="Envoyer">
+							<input type="submit" class="button"  value="Annuler">
+							
+						</div>
+						<p class="thank-you"> Merci d'avoir renseigner ce formulaire </p>
+
+					<!-- <div class="row">
+						<div class="col-md-6">
+							<label yourage>Age <span class="red"> * </span></label>
+							<input type="text"  id="age" name="age" class="form-control" placeholder="votre age">
+							<p class="comments"> Veuillez bien enregistrer votre Age svp! </p>
+						</div>
+						<div class="col-md-6">
+							<label form="youremail">Email <span class="red"> * </span></label>
+							<input type="text" id="email" name="email" class="form-control" placeholder="votre email">
+							<p class="comments"> Veuillez bien enregistrer votre Email svp! </p>
+						</div>
+						
+					</div> -->
+					
+				</form>
+			</div>
+		</div>
+	
+</div>
+</body>
+</html>
